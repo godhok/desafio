66ee77e90c9c84e23e0c14f92ccca5e7eec0acb9
godhok <victorlucasporto@gmail.com>
2022-06-17 16:31:37 -0300

    interação com javascript completa

---
+++ MMC/main.js
@@ -0,0 +1,13 @@
+function media() {
+  var numberone = parseFloat(document.getElementById('numberone').value)
+  var numbertwo = parseFloat(document.getElementById('numbertwo').value)
+  var numberthree = parseFloat(document.getElementById('numberthree').value)
+
+  var resultado = (numberone + numbertwo + numberthree) / 3
+  typeof resultado
+
+  document.getElementById(
+    'resultado'
+  ).innerText = `O resultado entre ${numberone}, ${numbertwo} e ${numberthree} é ${resultado}`
+  console.log(resultado)
+}
