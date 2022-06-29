1c135a4c1ab14afef5762b914ad579d86e8aad93
Lucas Porto <victorlucasporto@gmail.com>
2022-06-29 12:50:10 -0300

    começei a estudar manipulação de string

---
+++ DIVERSOS/strings.js
@@ -0,0 +1,8 @@
+var string = 'a string'
+console.log(string)
+console.log(string.length)
+
+console.log(string[0])
+console.log(string[string.length-1])
+
+https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/First_steps/Useful_string_methods
