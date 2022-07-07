007616faece4df3acdf800b707d0c930ce6cbfd0
godhok <victorlucasporto@gmail.com>
2022-07-07 16:56:57 -0300

    estudo sobre strings iniciado

--- DIVERSOS/strings.js
+++ DIVERSOS/strings.js
@@ -1,8 +1,43 @@
-var string = 'a string'
-console.log(string)
-console.log(string.length)
+/* STRINGs */
 
-console.log(string[0])
-console.log(string[string.length-1])
+//DEFININDO variavel = string
+var string = 'palavras'
+// ou talvez
+const string2 = 'numeros'
 
-https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/First_steps/Useful_string_methods
+console.log(string2)
+
+// CONVERTENDO ALGO EM STRING
+const NANshi = 106
+const convertido = new String(NANshi)
+console.log(convertido)
+
+/* Um pouco dificil entender o conceito de new
+    já que não sei orientado a objetos
+    Mas vamos lá!
+
+   Defini essa constante NANshi,
+   mas estou com problema, pois ela é sim um numero.
+
+   Pra resolver essa questão, usei a definicao de uma segunda variavel recebendo:
+
+   new - pra criar uma instancia da 'classe' do obj
+   e a função String() convertendo o parametro definido (Minha constante anterior)
+*/
+
+// Outra forma de converter tipos basicos é direto no console
+const num = 500
+console.log(num.toString())
+console.log(typeof num.toString())
+
+// const APegadinhadoMalando = "meu terminal exibe string sem aspas"
+
+// USANDO STRINGS
+// CONCATENANDO (juntando né)
+
+var nome = 'Lucas'
+var sobrenome = 'Porto'
+var nomeCompleto = 'Meu nome completo é : ' + nome + ' ' + sobrenome
+console.log(nomeCompleto)
+
+// https://www.alura.com.br/artigos/strings-com-javascript-o-que-sao-e-como-manipular?gclid=EAIaIQobChMI19uP0KfO-AIVyRvUAR35QgvgEAAYAyAAEgL01PD_BwE
